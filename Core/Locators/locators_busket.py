caddcc8e4841076f3e11d3e067e18519974bcd10
Pavlo <ppropaol@gmail.com>
2024-08-03 18:44:49 +0100

    Added new busket locators

---
+++ Core/Locators/locators_busket.py
@@ -0,0 +1,6 @@
+
+
+class BusketPageLocators:
+    def __init__(self):
+        self.product_added_to_busket_popup = '//p[contains(text(), "Produto adicionado ao carrinho")]'
+        self.busket_icon_on_top_of_page = '//li[@class="main-nav__cart"]'
